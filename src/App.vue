8bd18febf79ca08e9902a93a1b8f035c96c719aa
1asagne <74735618+1asagne@users.noreply.github.com>
2022-04-28 19:47:31 +0300

    Init vue app

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header></header>
+  <main></main>
+</template>
+
+<style>
+@import "./assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+</style>
